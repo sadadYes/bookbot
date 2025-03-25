2c75928056edb02b24dce041e23fae30e86250d5
sadadYes <syaddadpunya@gmail.com>
2025-03-25 21:30:11 +0700

    feat: add argument support

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import *
+import sys
 
 
 def get_book_text(dir) -> str:
@@ -7,22 +8,26 @@ def get_book_text(dir) -> str:
 
 
 def main():
-    book = 'books/frankenstein.txt'
-    contents = get_book_text(book)
-    word_counts = words_counter(contents)
-    char_counts = char_counter(contents)
-    sorted = sort_dict(char_counts)
+    if len(sys.argv) <= 1:
+        print('Usage: python3 main.py <path_to_book>')
+        sys.exit(1)
+    else:
+        book = sys.argv[1]
+        contents = get_book_text(book)
+        word_counts = words_counter(contents)
+        char_counts = char_counter(contents)
+        sorted = sort_dict(char_counts)
 
-    print('============ BOOKBOT ============')
-    print(f'Analyzing book found at {book}')
-    print('----------- Word Count ----------')
-    print(f'Found {word_counts} total words')
-    print('--------- Character Count -------')
+        print('============ BOOKBOT ============')
+        print(f'Analyzing book found at {book}')
+        print('----------- Word Count ----------')
+        print(f'Found {word_counts} total words')
+        print('--------- Character Count -------')
 
-    for i in sorted:
-        print(f'{i["char"]}: {i["count"]}')
+        for i in sorted:
+            print(f'{i["char"]}: {i["count"]}')
 
-    print('============= END ===============')
+        print('============= END ===============')
 
 
 if __name__ == '__main__':
