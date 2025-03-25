d0e388e277cc45dadf9c137f1af6c44cadf90782
sadadYes <syaddadpunya@gmail.com>
2025-03-25 21:16:00 +0700

    feat: add print report

--- main.py
+++ main.py
@@ -11,9 +11,18 @@ def main():
     contents = get_book_text(book)
     word_counts = words_counter(contents)
     char_counts = char_counter(contents)
+    sorted = sort_dict(char_counts)
 
-    print(f'{word_counts} words found in the document')
-    print(char_counts)
+    print('============ BOOKBOT ============')
+    print(f'Analyzing book found at {book}')
+    print('----------- Word Count ----------')
+    print(f'Found {word_counts} total words')
+    print('--------- Character Count -------')
+
+    for i in sorted:
+        print(f'{i["char"]}: {i["count"]}')
+
+    print('============= END ===============')
 
 
 if __name__ == '__main__':
--- stats.py
+++ stats.py
@@ -7,7 +7,14 @@ def char_counter(string) -> dict:
     char_counts = {}
     for i in string.lower():
         char_counts[i] = char_counts.get(i, 0) + 1
-    return char_counts
+    return [{'char': k, 'count': v} for k, v in char_counts.items()]
+
+
+def sort_dict(char_list) -> dict:
+    def sorting(d):
+        return d['count']
+    char_list.sort(reverse=True, key=sorting)
+    return char_list
 
 
 if __name__ == '__main__':
